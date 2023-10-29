69888567a6a631cb984be039c242646046b407b5
Iva Petkova <iva1petkova@gmail.com>
2023-10-29 23:22:20 +0200

    create converter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const currencies = ["USD", "EUR", "CAD", "BGN", "INR"];
+
+export default function App() {
+  const [amount, setAmount] = useState("");
+  const [output, setOutput] = useState(0.0);
+  const [inputCurrency, setInputCurrency] = useState("EUR");
+  const [outputCurrency, setOutputCurrency] = useState("BGN");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(
+    function () {
+      const controller = new AbortController();
+      async function calculateOutput() {
+        try {
+          const result = await fetch(
+            `https://api.frankfurter.app/latest?amount=${amount}&from=${inputCurrency}&to=${outputCurrency}`,
+            { signal: controller.signal }
+          );
+
+          if (!result.ok) {
+            throw new Error(
+              "Something went wrong while fetching the amount! :("
+            );
+          }
+          const data = await result.json();
+          if (!data || !data.rates || !(outputCurrency in data.rates)) {
+            throw new Error("No amount found! :(");
+          }
+          setOutput(data.rates[outputCurrency]);
+          setError("");
+        } catch (err) {
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      if (inputCurrency === outputCurrency) {
+        return setOutput(amount);
+      }
+      calculateOutput();
+
+      //Cleanup function
+      return function () {
+        controller.abort();
+      };
+    },
+    [amount, inputCurrency, outputCurrency]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <input
+        type="number"
+        min=""
+        step="0.01"
+        placeholder="0.00"
+        onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={isLoading}
+      />
+      <select
+        value={inputCurrency}
+        onChange={(e) => {
+          setInputCurrency(e.target.value);
+        }}
+        disabled={isLoading}
+      >
+        {currencies.map((c) => (
+          <option value={c}>{c}</option>
+        ))}
+      </select>
+      <select
+        value={outputCurrency}
+        onChange={(e) => {
+          setOutputCurrency(e.target.value);
+        }}
+        disabled={isLoading}
+      >
+        {currencies.map((c) => (
+          <option value={c}>{c}</option>
+        ))}
+      </select>
+      {isLoading && <Loader />}
+      {!isLoading && !error && (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {output.toFixed(2)} {outputCurrency}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
+      {error && <ErrorMessage message={error} />}
     </div>
   );
 }
 
-export default App;
+function Loader() {
+  return <p className="loader">Loading...</p>;
+}
+
+function ErrorMessage({ message }) {
+  return (
+    <p className="error">
+      <span>⛔</span> {message}
+    </p>
+  );
+}
